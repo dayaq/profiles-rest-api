7f150125c3b8d100857aaf3a2950c3b51c6338d1
Lateef <latbabs247@yahoo.com>
2023-12-11 00:10:01 +0100

    created django project and app

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
-    'rest-framework.authtoken',
+    'rest_framework',
+    'rest_framework.authtoken',
     'profiles_api',
 ]
 
